54dc4564d62e96cf0e7eabf17506ab83173f20de
rmesser87 <33704927+rmesser87@users.noreply.github.com>
2018-06-05 19:32:14 -0500

    more code added

--- server.js
+++ server.js
@@ -33,12 +33,12 @@ mongoose.connect("mongodb://localhost/week18Populater");
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with request
-  axios.get("http://www.echojs.com/").then(function(response) {
+  axios.get("http://www.swamplot.com/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    $("h2.entry-title").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
