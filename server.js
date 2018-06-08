ec6b7ac7c17f030e01e68eaaefd916b65ca12f2b
rmesser87 <33704927+rmesser87@users.noreply.github.com>
2018-06-08 18:56:38 -0500

    mongolab

--- server.js
+++ server.js
@@ -25,8 +25,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
+// If deployed, use the deployed database. Otherwise use the local swamplotscraper database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/swamplotscraper";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/swamplotscraper");
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 // Routes
 
